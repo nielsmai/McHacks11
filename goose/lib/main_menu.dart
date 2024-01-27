6b96f4a7f279d9465ffdc07e27be039d00b33d9f
Niels <77604434+nielsmai@users.noreply.github.com>
2024-01-27 17:43:01 -0500

    Added tutorial code from google codelabs

---
+++ goose/lib/main_menu.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MainMenuScreen extends StatelessWidget {
+  const MainMenuScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text('Main Menu'),
+    );
+  }
+}
