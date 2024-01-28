a58e5ca3944bd5d3d6b785805713354961842f3f
Niels <77604434+nielsmai@users.noreply.github.com>
2024-01-28 11:44:36 -0500

    Added many squirrels

--- Unity/McHacks/Assets/AIScript.cs
+++ Unity/McHacks/Assets/AIScript.cs
@@ -6,6 +6,24 @@ public class AIScript : MonoBehaviour
     public float range; // the range of the enemy detection
     public Transform player; // the reference to the player transform
 
+
+    private Object thisObject;
+
+    private void Awake()
+    {
+        thisObject = GetComponent<Object>();
+        // Set default player health to 3
+    }
+
+    private void Start()
+    {
+        // Set the player health to 3 if it is not set
+        if (!PlayerPrefs.HasKey(thisObject.ID))
+        {
+            PlayerPrefs.SetInt(thisObject.ID, 3);
+        }
+    }
+
     void Update()
     {
         // get the distance between the enemy and the player
@@ -28,6 +46,28 @@ public class AIScript : MonoBehaviour
                 // face left
                 transform.localScale = new Vector3(-1, 1, 1);
             }
+        }
+    }
+
+    // this method is called when the enemy collides with another object
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("PlayerController"))
+        {
+            //Decrement player health
+            // collision.GetComponent<PlayerMovement>().HeartCount--;
+            // Get the player's health
+            int playerHealth = (int) collision.GetComponent<PlayerMovement>().HeartCount;
+            // Decrement the player's health
+            playerHealth--;
+            Debug.Log(playerHealth);
+            // Update the player's health
+            collision.GetComponent<PlayerMovement>().HeartCount = playerHealth;
+            // Set PlayerPrefs to the new health
+            PlayerPrefs.SetInt(thisObject.ID, playerHealth);
+            // PlayerPrefs.SetInt(thisObject.ID, PlayerPrefs.GetInt(thisObject.ID) - 1);
+            //Destroy the enemy
+            Destroy(gameObject);
         }
     }
 }
