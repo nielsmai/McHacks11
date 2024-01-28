4c6af99614bb1ef09dcc9c2cb3303f32b7048f52
Niels <77604434+nielsmai@users.noreply.github.com>
2024-01-27 20:21:35 -0500

    Map.png added to assets

--- goose/lib/src/config.dart
+++ goose/lib/src/config.dart
@@ -13,7 +13,8 @@ const brickColors = [
   Color(0xff577590),
 ];
 
-const gameWidth = 820.0;
+// const gameWidth = 820.0;
+const gameWidth = 1600.0;
 const gameHeight = 1600.0;
 const ballRadius = gameWidth * 0.02;
 const batWidth = gameWidth * 0.2;
