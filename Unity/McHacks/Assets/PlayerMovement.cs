483e16388368cb116f399b56d1eba29bb37a7f98
jessLii <qian.q.li@mail.mcgill.ca>
2024-01-28 01:52:36 -0500

    resized map. map borders working

--- Unity/McHacks/Assets/PlayerMovement.cs
+++ Unity/McHacks/Assets/PlayerMovement.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f; 
+    public float moveSpeed = 5f;
     public Rigidbody2D rb;
     public Animator animator;
+
+    public GameObject topRightLimitGameObject;
+    public GameObject bottomLeftLimitGameObject;
+
+    private Vector3 topRightLimit;
+    private Vector3 bottomLeftLimit;
 
     // Add camera follow
     // public Camera cam;
     public Vector3 offset; // The distance between the camera and the player
 
     Vector2 movement;
+
+    void Start()
+    {
+        topRightLimit = topRightLimitGameObject.transform.position;
+        bottomLeftLimit = bottomLeftLimitGameObject.transform.position;
+    }
 
 
     // Update is called once per frame
@@ -28,9 +40,12 @@ public class PlayerMovement : MonoBehaviour
         animator.SetFloat("Speed", movement.sqrMagnitude);
 
         // Flip the sprite when moving left
-        if (movement.x < 0) {
+        if (movement.x < 0)
+        {
             transform.localScale = new Vector3(-1, 1, 1);
-        } else if (movement.x > 0) {
+        }
+        else if (movement.x > 0)
+        {
             transform.localScale = new Vector3(1, 1, 1);
         }
 
@@ -38,10 +53,23 @@ public class PlayerMovement : MonoBehaviour
         // cam.transform.position = transform.position + offset;
     }
 
-    void FixedUpdate() {
+    void FixedUpdate()
+    {
+
+        // Stop the sprite from going out of borders
+        if ((rb.position.x <= bottomLeftLimit.x && movement.x == -1) || (rb.position.x >= topRightLimit.x && movement.x == 1))
+        {
+            movement.x = 0;
+        }
+
+        if ((rb.position.y <= bottomLeftLimit.y && movement.y == -1) || (rb.position.y >= topRightLimit.y && movement.y == 1))
+        {
+            movement.y = 0;
+        }
+
         //Movement
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
         // Debug.Log(rb.position);
-        
+
     }
 }
